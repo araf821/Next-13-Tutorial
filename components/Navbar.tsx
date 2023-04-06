08bba1b2a5f0d890120a2bc45985c26dcc8331cb
araf821 <araf.ahmed200@gmail.com>
2023-04-06 03:02:55 -0400

    initialized our project structure

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+"use-client";
+
+import { motion } from "framer-motion";
+
+import styles from "@/styles";
+
+import { navVariants } from "@/utils/motion";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+export default Navbar;
