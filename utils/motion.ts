084b5b0d15141d9d70a84f2d6a2a0a146e5f94d4
araf821 <araf.ahmed200@gmail.com>
2023-04-07 02:21:35 -0400

    implemented more animations and hero section

--- utils/motion.ts
+++ utils/motion.ts
@@ -18,3 +18,51 @@ export const navVariants = {
     },
   },
 };
+
+export const slideIn = (
+  direction: string,
+  type: string,
+  delay: number,
+  duration: number
+) => ({
+  hidden: {
+    x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+    y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+  },
+  show: {
+    x: 0,
+    y: 0,
+    transition: {
+      type,
+      delay,
+      duration,
+      ease: "easeOut",
+    },
+  },
+});
+
+export const staggerContainer = (staggerChildren: any, delayChildren: any) => ({
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren,
+      delayChildren,
+    },
+  },
+});
+
+export const textVariant = (delay: any) => ({
+  hidden: {
+    y: 50,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 1.25,
+      delay,
+    },
+  },
+});
