1044a4fd1f9243c736a7ea87f9d24ef04fc05128
araf821 <araf.ahmed200@gmail.com>
2023-04-07 00:53:17 -0400

    added images to be used, initialized hero comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 const Page = () => {
   return (
-    <div className="bg-primary-black overflow-hidden" >
+    <div className="bg-primary-black overflow-hidden">
       <Navbar />
+      <Hero />
     </div>
   );
 };
+
 export default Page;
