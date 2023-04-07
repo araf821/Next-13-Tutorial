205f9c414715cb639c5e193e3946628dba44333a
araf821 <araf.ahmed200@gmail.com>
2023-04-07 18:09:56 -0400

    implemented about section

--- components/About.tsx
+++ components/About.tsx
@@ -19,6 +19,28 @@ const About = () => {
         className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
       >
         <TypingText title="| About Metaversus" textStyles="text-center" />
+
+        <motion.p
+          variants={fadeIn("up", "tween", 0.2, 1)}
+          className="mt-2 font-normal sm:text-8 text-[20px] text-center text-secondary-white"
+        >
+          <span className="font-extrabold">Metaverse</span> is a new thing in
+          the future, where you can enjoy the virtual world by feeling like
+          it&apos;s really real, you can feel what you feel in this metaverse
+          world, because this is really the{" "}
+          <span className="font-extrabold">madness of the metaverse</span> of
+          today, using only <span className="font-extrabold">VR</span> devices
+          you can easily <span className="font-extrabold">explore</span> the
+          metaverse world you want, turn your dreams into reality. Let&apos;s
+          explore the madness of the metaverse by scrolling down.
+        </motion.p>
+
+        <motion.img
+          variants={fadeIn("up", "tween", 0.3, 1)}
+          src="/arrow-down.svg"
+          alt="arrow down"
+          className="w-[18px] h-[28px] object-contain mt-[28px]"
+        />
       </motion.div>
     </section>
   );
