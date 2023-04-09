204749a5344c94aaa65ec957d2c9e1a07ee26337
araf821 <araf.ahmed200@gmail.com>
2023-04-08 20:55:14 -0400

    rendered the card + animations

--- components/ExploreCard.tsx
+++ components/ExploreCard.tsx
@@ -22,6 +22,24 @@ const ExploreCard: React.FC<CardProps> = ({
   active,
   handleClick,
 }) => {
-  return <div>ExploreCard</div>;
+  return (
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.4, 0.75)}
+      className={`${
+        active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
+      } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+    >
+      <img
+        src={imgUrl}
+        alt="title"
+        className="relative w-full h-full object-cover rounded-[24px]"
+      />
+      {active !== id && (
+        <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]" >
+          {title}
+        </h3>
+      )}
+    </motion.div>
+  );
 };
 export default ExploreCard;
